b034935a70d0ef0a9e1085d81daa33e6c7c7fe6a
Tina <tchen9@stuy.edu>
2018-02-28 14:49:36 -0500

    started

---
+++ svg.js
@@ -0,0 +1,14 @@
+var svg = document.getElementByID("svg");
+
+var draw = document.getElementByID("draw");
+var clear = document.getElementByID("clear");
+
+var drawCircle = function(){
+}
+
+draw.addEventListener("click", drawCircle);
+
+var clear = function(){
+}
+
+clear.addEventListener("click", clear);
