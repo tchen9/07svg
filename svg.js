6ea6e27f76da9e1d8266a8cce9f63cd3ae2df38f
Tina <tchen9@stuy.edu>
2018-02-28 23:53:17 -0500

    updated

--- svg.js
+++ svg.js
@@ -1,14 +1,51 @@
-var svg = document.getElementByID("svg");
+var svg = document.getElementById("s");
 
-var draw = document.getElementByID("draw");
-var clear = document.getElementByID("clear");
+var clear = document.getElementById("clear");
 
-var drawCircle = function(){
+var first = true;
+var px = -1;
+var py = -1;
+
+var drawCircle = function(e){
+    var x = e.offsetX;
+    var y = e.offsetX;
+    
+    if (first == true){
+	px = x;
+	py = y;
+	var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+	c.setAttribute("cx", x);
+	c.setAttribute("cy", y);
+	c.setAttribute("r", 20);
+	c.setAttribute("fill", "black");
+	svg.appendChild(c);
+	first = false;
+    }
+    
+    else{
+	var line = document.createElementNS("http://www.w3.org/2000/svg", "line");
+	line.setAttribute("x1", px);
+	line.setAttribute("y1", py);
+	line.setAttribute("x2", x);
+	line.setAttribute("y2", y);
+	line.setAttribute("stroke", "black");
+	svg.appendChild(line);
+	
+	var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+	c.setAttribute("cx", x);
+	c.setAttribute("cy", y);
+	c.setAttribute("r", 20);
+	svg.appendChild(c);
+
+	px = x;
+	py = y;
+    }
 }
 
-draw.addEventListener("click", drawCircle);
+svg.addEventListener("click", drawCircle);
 
-var clear = function(){
+var erase = function(){
+    svg.innerHTML = "";
 }
 
-clear.addEventListener("click", clear);
+clear.addEventListener("click", erase);
